ea93d209a8e225f19fe762d3a596cc070553644a
mohsen205 <mohsenmnassri7@gmail.com>
2023-11-26 15:33:47 +0100

    finish the layout of dashboard

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,18 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php 
+session_start();
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+// Check if the user is not logged in, redirect to the login page
+if (!isset($_SESSION["admin_id"]) || !isset($_SESSION["username"])) {
+    header("Location: ../index.php");
+    exit();
+}
+  // Get the value of the username session variable
+  $username = $_SESSION["username"];
+  
+require_once("../includes/header.php"); 
+?>
 
-<body>
-    <?php 
+<body class="sb-nav-fixed">
+    <?php require_once("../includes/dashboardLayout.php"); ?>
+    <div id="layoutSidenav_content">
+        <main>
+            <div class="container-fluid px-4">
+                <h1 class="mt-4">Tableau de bord</h1>
+                <ol class="breadcrumb mb-4">
+                    <li class="breadcrumb-item active">Bienvenue <strong class="text-capitalize">
+                            <?php echo $username ?> </strong></li>
+                </ol>
+            </div>
 
-    echo("Hello");
-    
-     ?>
+
+
+        </main>
+    </div>
+    </div>
 </body>
-
-</html>
+<?php require_once("../includes/footer.php") ?>
