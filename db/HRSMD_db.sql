d1f035a4a2c0abdaf8b80cc8d3e6a3c4c0664a1e
mohsen205 <mohsenmnassri17@gmail.com>
2024-01-02 06:57:59 +0100

    update database

--- db/HRSMD_db.sql
+++ db/HRSMD_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 05, 2023 at 04:30 PM
+-- Generation Time: Jan 02, 2024 at 06:56 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -30,6 +30,8 @@ SET time_zone = "+00:00";
 CREATE TABLE `admintb` (
   `id` int(11) NOT NULL,
   `username` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `role` varchar(50) NOT NULL DEFAULT 'admin',
+  `status` varchar(25) NOT NULL DEFAULT 'pending',
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `updatedAt` datetime NOT NULL
@@ -39,8 +41,16 @@ CREATE TABLE `admintb` (
 -- Dumping data for table `admintb`
 --
 
-INSERT INTO `admintb` (`id`, `username`, `password`, `createdAt`, `updatedAt`) VALUES
-(2, 'ameni.kohila', '$2y$10$qn.VCto1yGp4R7NkO7ZcU.XIzvUCDFk7qQqSLLmOp3FTP0p1puKh.', '2023-11-26 13:17:46', '2023-11-26 13:23:14');
+INSERT INTO `admintb` (`id`, `username`, `role`, `status`, `password`, `createdAt`, `updatedAt`) VALUES
+(2, 'ameni.kohila', 'super_admin', '0', '$2y$10$qn.VCto1yGp4R7NkO7ZcU.XIzvUCDFk7qQqSLLmOp3FTP0p1puKh.', '2024-01-01 17:27:09', '2023-11-26 13:23:14'),
+(3, 'revini', 'doctor', 'pending', '$2y$10$cAHXssoZuCDmvheuH0cnZORegmIaCI43aAGsS5yXJtCkzRxT692My', '2024-01-01 17:22:31', '0000-00-00 00:00:00'),
+(4, 'najugyxaly', 'nurse', 'pending', '$2y$10$PQy6kzCibumvyxZW2kIABOlcynaM6CnFQp4Heqmm9t.NHwqxVbSAW', '2024-01-01 17:27:41', '0000-00-00 00:00:00'),
+(5, 'vojapo', 'pharmacist', 'pending', '$2y$10$O/gqvBpokf5.qBVQKd2qu.98NRD1jkKlkwn08sxZ5dRBIylWpngjK', '2024-01-01 17:28:09', '0000-00-00 00:00:00'),
+(6, 'sovunu', 'pharmacist', 'pending', '$2y$10$4yzeuAtuoFODgtItqiR30ubhi8u7CGAMxxHMaF9QpGu2fs.csncZa', '2024-01-02 05:16:32', '0000-00-00 00:00:00'),
+(7, 'dywolamom', 'pharmacist', 'approved', '$2y$10$agdrQ/iWsSqwLzHjDRG13.jwe928Pgr1a2ZEehFJ0Npgla0Ai0aq.', '2024-01-02 05:52:06', '0000-00-00 00:00:00'),
+(8, 'ravylewih', 'pharmacist', 'approved', '$2y$10$.Ns0mMZAq7nG4v58tepsfua0LthnXV8M1RQXIRPbjipHW1rGQjGWe', '2024-01-02 05:21:58', '0000-00-00 00:00:00'),
+(10, 'ameni', 'doctor', 'approved', '$2y$10$BS8kweacJXRmSmIuwjVdnu0s95QJiBf5wRrkOwUwMja9RAbtpWt2O', '2024-01-02 05:52:48', '0000-00-00 00:00:00'),
+(11, 'ameni 2', 'pharmacist', 'approved', '$2y$10$KBnOe4fkZx39BUNqtWExe.eiLvcWp4v4.wUGRo7Bw31z64m5C65S6', '2024-01-02 05:53:50', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -57,16 +67,6 @@ CREATE TABLE `bedtb` (
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `updatedAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `bedtb`
---
-
-INSERT INTO `bedtb` (`id`, `roomName`, `bedNumber`, `description`, `status`, `createdAt`, `updatedAt`) VALUES
-(8, 'Bree Park', 350, 'Adipisci velit illu', 'occupied', '2023-12-05 15:19:24', '0000-00-00 00:00:00'),
-(9, 'Zorita Guy', 498, 'Unde itaque consecte', 'occupied', '2023-12-05 15:19:28', '0000-00-00 00:00:00'),
-(10, 'Adria Bush', 892, 'Officiis quia commod', 'available', '2023-12-05 15:19:33', '0000-00-00 00:00:00'),
-(11, 'India Mayo', 137, 'Nostrud omnis aliqua', 'available', '2023-12-05 15:19:37', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -122,15 +122,6 @@ CREATE TABLE `patient` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `patient`
---
-
-INSERT INTO `patient` (`id`, `fname`, `lname`, `gender`, `email`, `contact`, `bedId`, `doctorId`, `fees`, `date`, `time`, `patientStatus`, `doctorStatus`, `description`, `createdAt`, `updatedAt`) VALUES
-(1, 'Ivan Franco', 'Elijah Petersen', 'Female', 'qaqisoxyku@mailinator.com', 'Exercitationem repud', 8, 3, 0.00, '1997-02-04', '04:11:00', 'Admitted', 'Available', 'Consequatur Et cupi', '2023-12-05 15:28:47', '2023-12-05 15:28:47'),
-(2, 'Barclay Macias', 'Haley Barton', 'Male', 'kezolarefe@mailinator.com', 'Unde qui deserunt do', 11, 3, 0.00, '1993-09-21', '20:55:00', 'Admitted', 'Not Available', 'Eos dolorum amet f', '2023-12-05 15:28:51', '2023-12-05 15:28:51'),
-(3, 'Priscilla Burks', 'Hiram Richardson', 'Male', 'luvot@mailinator.com', 'Ad ab aliquip vel no', 8, 3, 0.00, '2003-01-20', '09:27:00', 'Discharged', 'Available', 'Ullamco laborum Qua', '2023-12-05 15:28:57', '2023-12-05 15:28:57');
-
---
 -- Indexes for dumped tables
 --
 
@@ -168,7 +159,7 @@ ALTER TABLE `patient`
 -- AUTO_INCREMENT for table `admintb`
 --
 ALTER TABLE `admintb`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `bedtb`
